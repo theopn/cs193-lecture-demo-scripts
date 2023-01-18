e2c61a16716e31fd52348c98e5a3dc6466dc69c9
Theo P <theo.park.n@gmail.com>
2023-01-18 17:48:29 -0500

    (lecture2) script and readme instruction

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# cs193-lecture-demo-scripts
+# CS193 Lecture Demo Scripts
+
+CS193: Tools is a Purdue University [CS course](https://selfservice.mypurdue.purdue.edu/prod/bzwsrch.p_catalog_detail?term=202320&subject=CS&cnbr=19300&enhanced=Y) designed to teach CS/DS/AI freshmen essential development tools such as UNIX shells, debugging tools, and Git.
+
+You can find resources I used for demos in CS193: Tools lectures while working as an instructor for two semesters. Future leads are welcome to clone or fork the repository to use it.
+
+## Lecture 2 - Essential Shell Commands
+
+1. Run `lecture2.sh` to initialize `~/cs193-demos/lecture2/` folder
+2. `ls` with flag (combined `-la`, etc.) and `clear`
+3. `cd c` (show the tab completion) and `pwd`
+4. `cd ..`, `touch helloWorld.java`, `mv hello_world.java java/`
+5. `mv ProjectVersionFinalFinalFinalActuallyFinal.java FinalProjectSubmission.java`
+
---
+++ lecture2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+make_folder() {
+  if [[ -e "$1" ]]; then
+    mkdir -p "$2/"
+    mv "$1" "$2"
+    echo "$1" already exists. Moving to "$2"
+  fi
+
+  mkdir -p "$1"
+}
+
+
+create_java_file() {
+  echo "public class "$1" {
+      public static void main(Strings[] args) {
+          System.out.printf(\"Hello world! This is %s file\n\", "$1");
+      }
+  }" > $2$1.java
+}
+
+
+create_c_file() {
+  echo "#include <stdio.h>
+
+  int main (int argc, char *argv[]) {
+    printf(\"Welcome to CS193! This is %s file\n\", "$1");
+    return 0;
+  }" > $2$1.c
+}
+
+
+create_cpp_file() {
+  echo "#include <iostream>
+
+  int main (int argc, char *argv[]) {
+    std::cout << \"Welcome to CS193! This is "$1" file\" << std::endl;
+    return 0;
+  }" > $2$1.cpp
+}
+
+main() {
+  make_folder ~/cs193-lecture-demo/lecture2 ~/cs193-lecture-demo-backup/
+
+  mkdir -p ~/cs193-lecture-demo/lecture2/java/
+  java_files=("HelloWorld" "ByeWorld" "ProjectVersionOne" "ProjectVersionTwo" "ProjectVersionFinal" "ProjectVersionFinalFinal" "ProjectVersionFinalFinalFinalActuallyFinal")
+  for file_name in ${java_files[@]}; do
+    create_java_file "$file_name" ~/cs193-lecture-demo/lecture2/java/
+  done
+
+  mkdir -p ~/cs193-lecture-demo/lecture2/c/
+  c_files=("doubly_linked_list" "signly_linked_list" "array_list" "malloc")
+  for file_name in ${c_files[@]}; do
+    create_c_file "$file_name" ~/cs193-lecture-demo/lecture2/c/
+  done
+
+  mkdir -p ~/cs193-lecture-demo/lecture2/cpp/
+  cpp_files=("HelloWorld" "ByeWorld" "WhatUpWorld")
+  for file_name in ${cpp_files[@]}; do
+    create_cpp_file "$file_name" ~/cs193-lecture-demo/lecture2/cpp/
+  done
+}
+
+main
