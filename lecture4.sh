2d2a2d475173d5d361fde6d58daf7d317166505d
Theo P <theo.park.n@gmail.com>
2023-04-06 12:18:04 -0400

    (lecture4) script cleanup

--- lecture4.sh
+++ lecture4.sh
@@ -2,30 +2,34 @@
 
 DEMO_PATH=~/cs193-lecture-demo/lecture4
 
-clean() {
+function clean() {
   if [[ -e $DEMO_PATH ]]; then
     rm -rf $DEMO_PATH
   fi
 }
 
-remote_setup() {
+function remote_setup() {
   mkdir -p $DEMO_PATH
-  remote_path=$DEMO_PATH"/remote"
+  remote_path=${DEMO_PATH}/remote
   mkdir -p $remote_path
-  touch $remote_path"/code.c"
-  echo -e "int main(void) {\n  /* Put some code in here*/\n  return 0;\n}" >> $remote_path"/code.c"
-  mkdir $remote_path"/folder_to_download/"
-  touch $remote_path"/folder_to_download/content.c"
-  echo -e "int main(void) {\n  char msg[50] = \"This is content.c\";\n  return 0;\n}" >> $remote_path"/folder_to_download/content.c"
+  touch ${remote_path}/code.c
+  echo -e "int main(void) {\n  /* Put some code in here*/\n  return 0;\n}" >> ${remote_path}/code.c
+  mkdir ${remote_path}/folder_to_download/
+  touch ${remote_path}/folder_to_download/content.c
+  echo -e "int main(void) {\n  char msg[50] = \"This is content.c\";\n  return 0;\n}" >> ${remote_path}/folder_to_download/content.c
+
+  echo "SSH demo setup for remote server complete"
 }
 
-local_setup() {
+function local_setup() {
   mkdir -p $DEMO_PATH
-  local_path=$DEMO_PATH"/local"
+  local_path=${DEMO_PATH}/local
   mkdir -p $local_path
-  mkdir $local_path"/folder_to_upload/"
-  touch $local_path"/folder_to_upload/other_code.c"
-  echo -e "int main(void) {\n  char msg[50] = \"This is other_code.c\";\n  return 0;\n}" >> $local_path"/folder_to_upload/other_code.c"
+  mkdir ${local_path}/folder_to_upload/
+  touch ${local_path}/folder_to_upload/other_code.c
+  echo -e "int main(void) {\n  char msg[50] = \"This is other_code.c\";\n  return 0;\n}" >> ${local_path}/folder_to_upload/other_code.c
+
+  echo "SSH demo setup for local machine complete"
 }
 
 main() {
